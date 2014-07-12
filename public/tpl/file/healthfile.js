3554e06da2aedc1e90e3fe49920be6acc38d04cc
payallmoney <payallmoney@163.com>
2014-07-13 03:16:18 +0800

    1,增加了树

--- public/tpl/file/healthfile.js
+++ public/tpl/file/healthfile.js
@@ -1,5 +1,56 @@
 var HealthFileCtrl = function ($scope, $http, $cookies, $location) {
     $scope.test = "测试";
+    $scope.treeOptions = {
+        nodeChildren: "children",
+        dirSelectable: true,
+        injectClasses: {
+            ul: "a1",
+            li: "a2",
+            liSelected: "a7",
+            iExpanded: "a3",
+            iCollapsed: "a4",
+            iLeaf: "a5",
+            label: "a6",
+            labelSelected: "a8"
+        }
+    };
+    $scope.dataForTheTree =
+        [
+            { "name" : "Joe", "age" : "21", "children" : [
+                { "name" : "Smith", "age" : "42", "children" : [] },
+                { "name" : "Gary", "age" : "21", "children" : [
+                    { "name" : "Jenifer", "age" : "23", "children" : [
+                        { "name" : "Dani", "age" : "32", "children" : [] },
+                        { "name" : "Max", "age" : "34", "children" : [] }
+                    ]}
+                ]}
+            ]},
+            { "name" : "Albert", "age" : "33", "children" : [] },
+            { "name" : "Ron", "age" : "29", "children" : [] }
+        ];
+    $scope.treedata =
+        [
+            { "label" : "User", "id" : "role1", "children" : [
+                { "label" : "subUser1", "id" : "role11", "children" : [] },
+                { "label" : "subUser2", "id" : "role12", "children" : [
+                    { "label" : "subUser2-1", "id" : "role121", "children" : [
+                        { "label" : "subUser2-1-1", "id" : "role1211", "children" : [
+
+                            { "label" : "subUser2-1-1", "id" : "role1211", "children" : [
+                                { "label" : "subUser2-1-1", "id" : "role1211", "children" : [
+
+
+                                ] }
+
+                            ] }
+                        ] },
+                        { "label" : "subUser2-1-2", "id" : "role1212", "children" : [] }
+                    ]}
+                ]}
+            ]},
+            { "label" : "Admin", "id" : "role2", "children" : [] },
+            { "label" : "Guest", "id" : "role3", "children" : [] }
+        ];
     $scope.tree = [
         {name: "Node", nodes: [
             {name: "Node", nodes: [
@@ -12,7 +63,13 @@ var HealthFileCtrl = function ($scope, $http, $cookies, $location) {
 
                                 {name: "Node", nodes: [
                                     {name: "Node", nodes: [
+                                        {name: "Node", nodes: [
 
+                                            {name: "Node", nodes: [
+
+
+                                            ]}
+                                        ]}
 
                                     ]}
 
