89d17703dc654c93ed918b8f4aa7e588f438e7ad
payallmoney <payallmoney@163.com>
2014-09-05 23:31:10 +0800

    1,完善app-radio

--- public/js/directives.js
+++ public/js/directives.js
@@ -5,41 +5,35 @@
 
 angular.module('myApp.directives', []).
     directive('appVersion', ['version', function (version) {
-        return function (scope, elm, attrs) {
+        return function ($scope, elm, attrs) {
             elm.text(version);
-        };
-    }]).
-    directive('appShowwhen', ['version', function (version) {
-        return function (scope, elm, attrs) {
-            $scope.$watch(attrs['appHidewhen'],function(value){
-                if(attrs['showvalue']==value){
-                    elm.show();
-                }else{
-                    elm.hide();
-                }
-            })
         };
     }]).
     directive('appRadio', [function () {
         return {
-            require: [ '^ngModel'],
-            transclude: true,
+            require: 'ngModel',
+//            transclude: true,
             restrict: 'A',
             scope: {
                 codetype: '@',
                 codename: '@ngModel',
-                modelval :"=ngModel",
-                default:'@'
+                modelval: "=ngModel",
+                default: '@'
             },
             controller: function ($scope, cache) {
                 $scope.codelist = cache.codelist[$scope.codetype].list;
-                if($scope.default){
-                    $scope.modelval = $scope.default;
+
+            },
+            link: function ($scope, element, attrs, modelctrl) {
+                console.log(modelctrl);
+                if ($scope.default) {
+                    $scope.modelval = {data:$scope.default};
+                }else{
+                    $scope.modelval = {data:$scope.null};
                 }
             },
             templateUrl: 'tpl/directivetpl/radio.html'
         };
-        return { link: link};
     }]).
     directive('appCheckbox', [function () {
         return {
@@ -49,28 +43,49 @@ angular.module('myApp.directives', []).
             scope: {
                 codetype: '@',
                 codename: '@ngModel',
-                modelval :"=ngModel",
+                modelval: "=ngModel",
                 uncheckval: '@',
-                default:'@'
+                default: '@'
             },
             controller: function ($scope, cache) {
                 $scope.codelist = cache.codelist[$scope.codetype].list;
-                if($scope.default){
+                if ($scope.default) {
                     $scope.modelval = {};
                     $scope.modelval[$scope.default] = true;
                 }
             },
-            link : function(scope, element, attrs,modelctrl){
-//                scope.$watch("modelval", function (newvalue, oldvalue) {
-//                    console.log(newvalue,oldvalue);
-//                    modelctrl.$setViewValue(newvalue);
-//                });
+            link: function ($scope, element, attrs, modelctrl) {
+                if ($scope.uncheckval) {
+                    $scope.$watch(function () {
+                        return $scope.modelval;
+                    }, function (newval, oldval) {
+                        var changed = null;
+                        var checked = '';
+                        $.each(newval, function (i, v) {
+                            if (v != oldval[i]) {
+                                changed = i;
+                            }
+                            if (v == true) {
+                                checked = checked + ',' + i;
+                            }
+                        });
+                        if (checked.length == 0) {
+                            newval[$scope.uncheckval] = true;
+                        }
+                        if (newval[changed] == true) {
+                            if (changed == $scope.uncheckval) {
+                                $.each(newval, function (i, v) {
+                                    newval[i] = false;
+                                });
+                                newval[$scope.uncheckval] = true;
+                            } else {
+                                newval[$scope.uncheckval] = false;
+                            }
+                        }
 
-                if(scope.uncheckval){
-
+                    }, true);
                 }
             },
             templateUrl: 'tpl/directivetpl/checkbox.html'
         };
-        return { link: link};
     }]);
